ebe2c39021be5876b26521ba2645ec2f925c9f00
c16192 <ishidashu@gmail.com>
2019-05-28 16:38:25 +0100

    Added snowboy and implemented hotword listen using snowboy

--- orion_asr/src/speech_server.py
+++ orion_asr/src/speech_server.py
@@ -8,18 +8,21 @@ from orion_actions.msg import SpeakAndListenAction, SpeakAndListenGoal, SpeakAnd
 
 from tmc_msgs.msg import TalkRequestAction, TalkRequestGoal, Voice
 import speech_recognition as sr
-# from wavenet.recognize import WaveNet
+from wavenet.recognize import WaveNet
 from recogniser import ASR
-import time
+import time, os
 import numpy as np
+
+from snowboy import snowboydecoder
 
 EXIT_WORDS = ["stop", "cancel", "finish", "exit", "terminate", "quit"]
 
 
 class SpeechServer(object):
     # create messages that are used to publish feedback/result
-    _feedback = SpeakAndListenFeedback()
-    _result = SpeakAndListenResult()
+    _snl_feedback = SpeakAndListenFeedback()
+    _snl_result = SpeakAndListenResult()
+    _hotword_result = HotwordListenResult()
 
     def __init__(self, name):
         self._action_name = name
@@ -37,8 +40,8 @@ class SpeechServer(object):
         
         rospy.logwarn("SpeechServer started:")
 
-        self._as = SimpleActionServer("speak_and_listen", SpeakAndListenAction, execute_cb=self.speak_and_listen_cb, auto_start=False)
-        self._as.start()
+        self._snl_as = SimpleActionServer("speak_and_listen", SpeakAndListenAction, execute_cb=self.speak_and_listen_cb, auto_start=False)
+        self._snl_as.start()
 
         self._hotword_as = SimpleActionServer("hotword_listen", HotwordListenAction, execute_cb=self.hotword_listen_cb, auto_start=False)
         self._hotword_as.start()
@@ -72,7 +75,7 @@ class SpeechServer(object):
         asr = ASR(self.recognizer, "self.wavenet")
 
         with sr.Microphone() as source:
-            print("Started recording...")
+            rospy.loginfo("Started recording...")
 
             timelimit = time.time() + timeout if timeout else np.inf
 
@@ -80,9 +83,9 @@ class SpeechServer(object):
 
             while timelimit - time.time() > 0:
 
-                if self._as.is_preempt_requested():
+                if self._snl_as.is_preempt_requested():
                     rospy.logwarn('%s: Preempted' % self._action_name)
-                    self._as.set_preempted()
+                    self._snl_as.set_preempted()
                     return
 
                 answer, param, confidence, transcription, succeeded = asr.main(source, candidates, params)
@@ -90,9 +93,9 @@ class SpeechServer(object):
                 if succeeded:
                     break
                 else:
-                    self._feedback.answer, self._feedback.param, self._feedback.confidence, self._feedback.transcription = answer, param, confidence, transcription
-                    self._feedback.remaining = timelimit - time.time() if timeout else 0
-                    self._as.publish_feedback(self._feedback)
+                    self._snl_feedback.answer, self._snl_feedback.param, self._snl_feedback.confidence, self._snl_feedback.transcription = answer, param, confidence, transcription
+                    self._snl_feedback.remaining = timelimit - time.time() if timeout else 0
+                    self._snl_as.publish_feedback(self._snl_feedback)
 
             if succeeded:
                 self.speak("OK. You said " + answer)
@@ -100,48 +103,53 @@ class SpeechServer(object):
                 self.speak("Sorry, I didn't get it.")
                 self.recognizer.adjust_for_ambient_noise(source)
 
-            self._result.answer, self._result.param, self._result.confidence, self._result.transcription, self._result.succeeded = answer, param, confidence, transcription, succeeded
-            self._as.set_succeeded(self._result)
+            self._snl_result.answer, self._snl_result.param, self._snl_result.confidence, self._snl_result.transcription, self._snl_result.succeeded = answer, param, confidence, transcription, succeeded
+            self._snl_as.set_succeeded(self._snl_result)
 
     def hotword_listen_cb(self, goal):
 
         timeout = goal.timeout
+        timelimit = time.time() + timeout if timeout else np.inf
 
         rospy.logwarn("HotwordListen action started:")
 
-        asr = ASR(self.recognizer, "self.wavenet")
+        hotword_options = ["bambam", "cancel"]
 
-        with sr.Microphone() as source:
+        detected = {"hotword": ""}
 
+        def detected_callback(ind):
+            detected["hotword"] = hotword_options[ind]
+            rospy.loginfo("Detected hotword: " + detected["hotword"])
+
+        def interrupt_callback():
             if self._hotword_as.is_preempt_requested():
                 rospy.logwarn('%s: Preempted' % self._action_name)
                 self._hotword_as.set_preempted()
-                return
+                return True
+            if timelimit - time.time() <= 0:
+                return True
+            return detected["hotword"] != ""
 
-            print("Started recording...")
+        dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "snowboy/resources")
+        models = [os.path.join(dir_path, s + '.pmdl') for s in hotword_options]
 
-            timelimit = time.time() + timeout if timeout else np.inf
+        detector = snowboydecoder.HotwordDetector(models, sensitivity=0.5)
+        rospy.loginfo('Started listening to hotwords...')
 
-            answer, confidence, succeeded = "", 0.0, False
+        # main loop
+        detector.start(detected_callback=detected_callback,
+                       interrupt_check=interrupt_callback,
+                       sleep_time=0.03)
 
-            while timelimit - time.time() > 0:
-                answer, _, confidence, _, succeeded = asr.main(source, EXIT_WORDS, [])
-                rospy.logwarn('Answer: %s, Confidence: %s' % (answer, confidence))
-                if succeeded:
-                    break
-                else:
-                    self._feedback.confidence = confidence
-                    self._feedback.remaining = timelimit - time.time() if timeout else 0
-                    self._hotword_as.publish_feedback(self._feedback)
+        detector.terminate()
 
-            if succeeded:
-                self.speak("OK. You said " + answer)
-            else:
-                self.speak("Sorry, I didn't get it.")
-                self.recognizer.adjust_for_ambient_noise(source)
+        if detected["hotword"]:
+            snowboydecoder.play_audio_file()
 
-            self._result.confidence, self._result.succeeded = confidence, succeeded
-            self._hotword_as.set_succeeded(self._result)
+        rospy.loginfo("Finishing hotword detection...")
+
+        self._hotword_result.hotword, self._hotword_result.succeeded = detected["hotword"], detected["hotword"] != ""
+        self._hotword_as.set_succeeded(self._hotword_result)
 
 
 if __name__ == '__main__':
