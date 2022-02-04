96d4623e8ee722075ae878f24234cddda6ce7ec1
Ricardo Cannizzaro <ricardo.cannizzaro@gmail.com>
2022-02-04 19:20:32 +0000

    added extra nltk resource dependency to dependency install script

--- orion_asr/scripts/download_nltk_resources.py
+++ orion_asr/scripts/download_nltk_resources.py
@@ -3,7 +3,7 @@
 import nltk
 
 if __name__ == "__main__":
-  resources = ['punkt', 'wordnet', 'brown']
+  resources = ['punkt', 'wordnet', 'brown', 'omw-1.4']
 
   print("Starting resource downloads")
   for resource in resources:
