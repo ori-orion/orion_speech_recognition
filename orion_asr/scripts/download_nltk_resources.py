2cf0dcc775486c93b86815f6331d1100da1c6566
shuishida <ishidashu@gmail.com>
2022-05-16 18:24:29 +0100

    Small fixes

--- orion_asr/scripts/download_nltk_resources.py
+++ orion_asr/scripts/download_nltk_resources.py
@@ -3,9 +3,9 @@
 import nltk
 
 if __name__ == "__main__":
-  resources = ['punkt', 'wordnet', 'brown', 'omw-1.4']
+    resources = ['punkt', 'wordnet', 'brown', 'omw-1.4']
 
-  print("Starting resource downloads")
-  for resource in resources:
-    nltk.download(resource) 
-  print("\nResource downloads complete")
+    print("Starting resource downloads")
+    for resource in resources:
+        nltk.download(resource)
+    print("\nResource downloads complete")
