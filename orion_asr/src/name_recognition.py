b22d32a4d1873b4ca7fcce7500e02781085dba30
shuishida <ishidashu@gmail.com>
2022-07-11 04:50:38 +0100

    Simplified token extraction

--- orion_asr/src/name_recognition.py
+++ orion_asr/src/name_recognition.py
@@ -6,11 +6,7 @@ def check_for_name(transcripts):
     # My name is.. I am... I'm ... *Just name*... They call me ... 
     for transcript in transcripts:
         tokens = nltk.word_tokenize(transcript)
-        # for I'm it returns [I, 'm] so we need just m
-        for i, word in enumerate(tokens):
-            if len(word.split("'")) > 1:
-                tokens[i] = word.split("'")[1]
-        tokens = [word.lower() for word in tokens if word.isalpha()]
+        tokens = [word.lower() if word.isalpha() or word == "'m" else word for word in tokens]
 
         name = ''
         if len(tokens) <= 2:
@@ -18,7 +14,7 @@ def check_for_name(transcripts):
             name = tokens[0].capitalize()
         else:
             for i, word in enumerate(tokens):
-                if word == 'is' or word == 'am' or word == "i'm" or word == "m" or word == "me":
+                if word == 'is' or word == 'am' or word == "'m" or word == "me":
                     # My name is NAME
                     try:
                         name = tokens[i + 1].capitalize()
