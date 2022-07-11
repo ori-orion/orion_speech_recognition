43bcba5b4fdf152e82a7aaa3d930e012d89f95cd
shuishida <ishidashu@gmail.com>
2022-07-11 04:42:37 +0100

    Minor formatting and try-catch to index error

--- orion_asr/src/name_recognition.py
+++ orion_asr/src/name_recognition.py
@@ -1,26 +1,30 @@
 import nltk
 
+
 def check_for_name(transcripts):
     # The function which checks if the sentence is one if the following and extracts the name:
     # My name is.. I am... I'm ... *Just name*... They call me ... 
     for transcript in transcripts:
         tokens = nltk.word_tokenize(transcript)
         # for I'm it returns [I, 'm] so we need just m
-        for i,word in enumerate(tokens):
-            if(len(word.split("'")) > 1):
+        for i, word in enumerate(tokens):
+            if len(word.split("'")) > 1:
                 tokens[i] = word.split("'")[1]
         tokens = [word.lower() for word in tokens if word.isalpha()]
 
         name = ''
-        if(len(tokens) == 1):
-            #If they only say one word that word is their name
+        if len(tokens) <= 2:
+            # If they only say one word that word is their name
             name = tokens[0].capitalize()
         else:
-            for i,word in enumerate(tokens):
-                if(word == 'is' or word == 'am' or word == "i'm" or word == "m" or word == "me"):
+            for i, word in enumerate(tokens):
+                if word == 'is' or word == 'am' or word == "i'm" or word == "m" or word == "me":
                     # My name is NAME
-                    name = tokens[i+1].capitalize()
-        if(name != ''):
+                    try:
+                        name = tokens[i + 1].capitalize()
+                    except IndexError:
+                        pass
+        if name:
             return name
     # If none of the transcripts returned a name:
-    return name
+    return ''
