4602954e8f02c241d0ac9c6b9a950318f331b327
Lee Chih Jung <54738966+LordJatonyas@users.noreply.github.com>
2023-01-31 09:26:19 +0000

    Arranged transcription into a function

--- orion_asr/src/transcriber.py
+++ orion_asr/src/transcriber.py
@@ -11,28 +11,32 @@ SetLogLevel(0)
 
 DEFAULT_MODEL_PATH = os.path.join(ROOT_DIR, "data", "vosk-model-small-en-us-0.15")
 
-if not os.path.exists(DEFAULT_MODEL_PATH):
-    print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-    exit(1)
+def transcribe():
+    if not os.path.exists(DEFAULT_MODEL_PATH):
+        print("Please download the model from https://alphacephei.com/vosk/model")
+        exit(1)
 
-wf = wave.open(sys.argv[1], "rb")
-if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-    print("Audio file must be in WAV format mono PCM.")
-    exit(1)
+    wf = wave.open(sys.argv[1], "rb")
+    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+        print("Audio file must be in WAV format mono PCM.")
+        exit(1)
 
-model = Model(DEFAULT_MODEL_PATH)
-rec - KaldiRecognizer(model, wf.getframerate())
-rec.SetWords(True)
+    model = Model(DEFAULT_MODEL_PATH)
+    rec - KaldiRecognizer(model, wf.getframerate())
+    rec.SetWords(True)
 
-while True:
-    data = wf.readframes(100)
-    if len(data) == 0:
-        break
-    """
-    if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
-    """
+    while True:
+        data = wf.readframes(100)
+        if len(data) == 0:
+            break
+        """
+        if rec.AcceptWaveform(data):
+            print(rec.Result())
+        else:
+            print(rec.PartialResult())
+        """
 
-print(rec.FinalResult())
+    print(rec.FinalResult())
+
+if __name__ == "__main__":
+    transcribe()
