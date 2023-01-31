facc9bcb8888b866a530c00f1a12c57a11dd1832
Lee Chih Jung <54738966+LordJatonyas@users.noreply.github.com>
2023-01-31 09:23:30 +0000

    Transcriber function added, only MONO PCM allowed

--- orion_asr/src/transcriber.py
+++ orion_asr/src/transcriber.py
@@ -1,85 +1,38 @@
+#!/usr/bin/env python3
+
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import sys
 import os
 import wave
-import queue
-import argparse
-import sounddevice as sd
-import vosk
-import sys
-from zipfile import ZipFile
-
 
 from constants import ROOT_DIR
 
-q = queue.Queue()
+SetLogLevel(0)
 
-def intORstr(text):
-    try:
-        return int(text)
-    except ValueError:
-        return text
+DEFAULT_MODEL_PATH = os.path.join(ROOT_DIR, "data", "vosk-model-small-en-us-0.15")
 
-def callback(indata, frames, time, status):
-    if status:
-        print(status, file=sys.stderr)
-    q.put(bytes(indata))
+if not os.path.exists(DEFAULT_MODEL_PATH):
+    print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+    exit(1)
 
-def main():
+wf = wave.open(sys.argv[1], "rb")
+if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    print("Audio file must be in WAV format mono PCM.")
+    exit(1)
 
-    parser = argparse.ArgumentParser(add_help=False)
-    args, remaining = parser.parse_known_args()
+model = Model(DEFAULT_MODEL_PATH)
+rec - KaldiRecognizer(model, wf.getframerate())
+rec.SetWords(True)
 
-    MODEL_PATH = os.path.join(ROOT_DIR, "data", "vosk-model-small-en-us-0.15")
+while True:
+    data = wf.readframes(100)
+    if len(data) == 0:
+        break
+    """
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+    """
 
-    parser.add_argument('-m','--model',type=str,metavar='MODEL_PATH',help='Path to model')
-    parser.add_argument('-r','--samplerate',type=int,help='sampling rate')
-    parser.add_argument('-d','--device',type=intORstr,help='input device (numeric ID or substring')
-    parser.add_argument('-f','--filename',type=str,metavar='FILENAME',help='audiofile to store recording to')
-
-    args = parser.parse_args(remaining)
-
-    try:
-        if not os.path.exists(MODEL_PATH):
-            if os.path.exists(MODEL_PATH + ".zip"):
-                print(f"Unpacking vosk model into {MODEL_PATH}")
-                with ZipFile(MODEL_PATH + ".zip", "r") as f:
-                    f.extractall(os.path.join(MODEL_PATH, ".."))
-            else:
-                raise Exception(f"""Please download a model for your language from https://alphacephei.com/vosk/models
-                and unpack at '{MODEL_PATH}'""")
-        if args.samplerate is None:
-            device_info = sd.query_devices(args.device, 'input')
-            args.samplerate = int(device_info['default_samplerate'])
-
-
-        model = vosk.Model(args.model)
-
-        if args.filename:
-            dump_fn = open(args.filename, "wb")
-        else:
-            dump_fn = None
-
-        with sd.RawInputStream(samplerate=args.samplerate,blocksize = 8000,device=args.device,dtype='int16',channels = 1, callback=callback):
-            print('Press Ctrl+C to stop recording')
-
-            rec = vosk.KaldiRecognizer(model, args.samplerate)
-            while True:
-                wavefile = wave.open("sound.wav","wb")
-                wavefile.setnchannels(1)
-                wavefile.setsampwidth(2)
-                wavefile.setframerate(args.samplerate)
-                data = q.get()
-                if rec.AcceptWaveform(data):
-                    print(rec.Result())
-                if dump_fn is not None:
-                    dump_fn.write(data)
-
-    except KeyboardInterrupt:
-        wavefile.close()
-        print('\nDone')
-        parser.exit(0)
-    except Exception as e:
-        parser.exit(type(e).__name__ + ': ' + str(e))
-
-
-if __name__ == '__main__':
-    main()
+print(rec.FinalResult())
